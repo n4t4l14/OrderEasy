6450e778af576dc346dd5454c8683867566ec67c
NataliaCastro <julieth.cp94@gmail.com>
2024-04-16 19:11:36 -0500

    se realiza creación de perfil de usuario y modificación de estilos visuales

--- app/src/main/java/com/example/ordereasy/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/ordereasy/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.ordereasy.ui.home
+package com.example.ordereasy.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.ordereasy.databinding.FragmentHomeBinding
+import com.example.ordereasy.databinding.FragmentProfileBinding
 
-class HomeFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,15 +23,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.textMyDataTitle
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
         return root
     }
 
--- app/src/main/java/com/example/ordereasy/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/ordereasy/ui/profile/ProfileViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.ordereasy.ui.home
+package com.example.ordereasy.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "profile image"
     }
     val text: LiveData<String> = _text
 }
